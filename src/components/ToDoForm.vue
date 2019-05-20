5682c6c5051fd96dd472c9fdd6082ff99db74b37
donuhead7 <kennethdu3@gmail.com>
2019-05-20 11:53:36 -0700

    init

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>To Do Form Test</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "to-do-form"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ToDoTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>To Do Table Test</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "to-do-table"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
